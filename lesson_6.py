0e18336a2a34d64bf420e4d0b372e4c74982f630
LexHerzBrennt <lexherzbrennt@gmail.com>
2023-02-13 10:53:34 +0300

    Added zadacha_43 to lesson_6.py

--- lesson_6.py
+++ lesson_6.py
@@ -9,4 +9,20 @@ def zadacha_41():
 
     print(f'You\'ve entered {count} numbers greater than 0')
 
+
+def zadacha_43():
+    print('Zadacha 43:')
+
+    b1 = int(input('b1: '))
+    k1 = int(input('k1: '))
+    b2 = int(input('b2: '))
+    k2 = int(input('k2: '))
+
+    x = (b2 - b1) / (k1 - k2)
+    y = k1 * x + b1
+
+    print(f'Coordinates: ({x};{y})')
+
+
 zadacha_41()
+zadacha_43()
