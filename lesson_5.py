5baba4ec7aaff25820d7b0c3c77cfe9d39e8f84c
LexHerzBrennt <lexherzbrennt@gmail.com>
2023-02-13 10:11:49 +0300

    Added zadacha_36 to lesson_5.py

--- lesson_5.py
+++ lesson_5.py
@@ -8,6 +8,20 @@ def zadacha_34():
         if i % 2 == 0:
             count += 1
 
-    print('Zadacha 34:', array, f'({count} even numbers)')
+    print('Zadacha 34:', array, f'(Even numbers: {count})')
+
+def zadacha_36():
+    from random import randint
+
+    array = [randint(1, 99) for i in range(5)]
+    sum = 0
+
+    #The loop sums a numbers on even INDEXES
+    for i in range(5):
+        if i % 2 == 0:
+            sum += array[i]
+
+    print('Zadacha 34:', array, f'(Sum: {sum})')
 
 zadacha_34()
+zadacha_36()
