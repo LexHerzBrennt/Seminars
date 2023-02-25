2c4b3018499b011d0002e8b5030f11d1ef65479f
LexHerzBrennt <lexherzbrennt@gmail.com>
2023-02-25 18:25:44 +0300

    Added zadacha_56 to lesson_8.py

--- lesson_8.py
+++ lesson_8.py
@@ -18,4 +18,24 @@ def zadacha_54(columns: int, rows: int):
         print(array[i])
 
 
+def zadacha_56(columns: int, rows: int):
+    print('Zadacha 56:')
+
+    from random import randint
+    array = [[randint(0, 9) for i in range(columns)] for i in range(rows)]
+
+    for i in range(rows):
+        print(array[i])
+
+    max_sum = 0
+    max_row = 0
+    for i in range(len(array)):
+        if max_sum < sum(array[i]):
+            max_sum = sum(array[i])
+            max_row = i
+
+    print(f'Result: {max_sum} (index of row: {max_row})')
+
+
 zadacha_54(5, 4)
+zadacha_56(5, 4)
